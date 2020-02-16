85e21fcec20dbc6074873f64c28952c22819d2e3
WYSIWYR <dnlwldnlf84@gmail.com>
2020-02-16 20:24:17 +0900

    mss-backend 초기화
    mss-backend 다시 설정

--- mss-backend/src/index.js
+++ mss-backend/src/index.js
@@ -2,14 +2,15 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use((ctx, next) => {
+app.use(async (ctx, next) => {
     console.log(ctx.url);
     console.log(1);
     if (ctx.query.authorized !== '1') {
         ctx.status = 401;
         return;
     }
-    next();
+    await next();
+    console.log('END');
 });
 
 app.use((ctx, next) => {
